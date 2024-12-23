17940c1d8147da8e35686bb90d0e2cf74e6d933c
guikominami <contato.mudalab@gmail.com>
2024-12-23 18:06:04 +0100

    build production

--- src/assets/data/ImagesData.js
+++ src/assets/data/ImagesData.js
@@ -1,67 +1,67 @@
 export const Drawings = [
   {
     id: 1,
-    url: "/drawings/drawings001.jpg",
+    url: "./drawings/drawings001.jpg",
     text: "Drawings 1",
   },
   {
     id: 2,
-    url: "/drawings/drawings002.jpg",
+    url: "./drawings/drawings002.jpg",
     text: "Aranha",
   },
   {
     id: 3,
-    url: "/drawings/drawings003.jpg",
+    url: "./drawings/drawings003.jpg",
     text: "Mergulhador",
   },
   {
     id: 4,
-    url: "/drawings/drawings004.jpg",
+    url: "./drawings/drawings004.jpg",
     text: "Tiozim",
   },
   {
     id: 5,
-    url: "/drawings/drawings005.jpg",
+    url: "./drawings/drawings005.jpg",
     text: "Panda",
   },
   {
     id: 6,
-    url: "/drawings/drawings006.jpg",
+    url: "./drawings/drawings006.jpg",
     text: "Vovó",
   },
   {
     id: 7,
-    url: "/drawings/drawings007.jpg",
+    url: "./drawings/drawings007.jpg",
     text: "Drawings 7",
   },
   {
     id: 8,
-    url: "/drawings/drawings008.jpg",
+    url: "./drawings/drawings008.jpg",
     text: "Drawings 8",
   },
   {
     id: 9,
-    url: "/drawings/drawings009.jpg",
+    url: "./drawings/drawings009.jpg",
     text: "Drawings 9",
   },
   {
     id: 10,
-    url: "/drawings/drawings010.jpg",
+    url: "./drawings/drawings010.jpg",
     text: "Drawings 10",
   },
   {
     id: 11,
-    url: "/drawings/drawings011.jpg",
+    url: "./drawings/drawings011.jpg",
     text: "Amy",
   },
   {
     id: 12,
-    url: "/drawings/drawings012.jpg",
+    url: "./drawings/drawings012.jpg",
     text: "Self",
   },
   {
     id: 13,
-    url: "/drawings/drawings013.jpg",
+    url: "./drawings/drawings013.jpg",
     text: "Self",
   },
 ];
@@ -69,22 +69,22 @@ export const Drawings = [
 export const Sketches = [
   {
     id: 1,
-    url: "/sketches/sketches001.jpg",
+    url: "./sketches/sketches001.jpg",
     text: "sketches 1",
   },
   {
     id: 2,
-    url: "/sketches/sketches002.jpg",
+    url: "./sketches/sketches002.jpg",
     text: "sketches 2",
   },
   {
     id: 3,
-    url: "/sketches/sketches003.jpg",
+    url: "./sketches/sketches003.jpg",
     text: "sketches 3",
   },
   {
     id: 4,
-    url: "/sketches/sketches004.jpg",
+    url: "./sketches/sketches004.jpg",
     text: "sketches 4",
   },
 ];
@@ -92,82 +92,82 @@ export const Sketches = [
 export const Digital = [
   {
     id: 1,
-    url: "/digital/digital001.jpg",
+    url: "./digital/digital001.jpg",
     text: "scare",
   },
   {
     id: 2,
-    url: "/digital/digital002.jpg",
+    url: "./digital/digital002.jpg",
     text: "square",
   },
   {
     id: 3,
-    url: "/digital/digital003.jpg",
+    url: "./digital/digital003.jpg",
     text: "square",
   },
   {
     id: 4,
-    url: "/digital/digital004.jpg",
+    url: "./digital/digital004.jpg",
     text: "square",
   },
   {
     id: 5,
-    url: "/digital/digital005.jpg",
+    url: "./digital/digital005.jpg",
     text: "square",
   },
   {
     id: 6,
-    url: "/digital/digital006.jpg",
+    url: "./digital/digital006.jpg",
     text: "monsters",
   },
   {
     id: 7,
-    url: "/digital/digital007.jpg",
+    url: "./digital/digital007.jpg",
     text: "monsters",
   },
   {
     id: 8,
-    url: "/digital/digital008.jpg",
+    url: "./digital/digital008.jpg",
     text: "monsters",
   },
   {
     id: 9,
-    url: "/digital/digital009.jpg",
+    url: "./digital/digital009.jpg",
     text: "monsters",
   },
   {
     id: 10,
-    url: "/digital/digital010.jpg",
+    url: "./digital/digital010.jpg",
     text: "monsters",
   },
   {
     id: 11,
-    url: "/digital/digital011.jpg",
+    url: "./digital/digital011.jpg",
     text: "monsters",
   },
   {
     id: 12,
-    url: "/digital/digital012.jpg",
+    url: "./digital/digital012.jpg",
     text: "monsters",
   },
   {
     id: 13,
-    url: "/digital/digital013.jpg",
+    url: "./digital/digital013.jpg",
     text: "monsters",
   },
   {
     id: 14,
-    url: "/digital/digital014.jpg",
+    url: "./digital/digital014.jpg",
     text: "monsters",
   },
   {
     id: 15,
-    url: "/digital/digital015.jpg",
+    url: "./digital/digital015.jpg",
     text: "monsters",
   },
   {
     id: 16,
-    url: "/digital/digital016.jpg",
+    url: "./digital/digital016.jpg",
     text: "monsters",
   },
 ];
