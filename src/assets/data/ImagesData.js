b9e16c870fda3127778267ef17dd497754a84bde
guikominami <contato.mudalab@gmail.com>
2024-12-29 19:25:33 +0100

    new import images mode

--- src/assets/data/ImagesData.js
+++ src/assets/data/ImagesData.js
@@ -1,67 +1,100 @@
+import drawings001 from "../imgs/drawings/drawings001.jpg";
+import drawings002 from "../imgs/drawings/drawings002.jpg";
+import drawings003 from "../imgs/drawings/drawings003.jpg";
+import drawings004 from "../imgs/drawings/drawings004.jpg";
+import drawings005 from "../imgs/drawings/drawings005.jpg";
+import drawings006 from "../imgs/drawings/drawings006.jpg";
+import drawings007 from "../imgs/drawings/drawings007.jpg";
+import drawings008 from "../imgs/drawings/drawings008.jpg";
+import drawings009 from "../imgs/drawings/drawings009.jpg";
+import drawings010 from "../imgs/drawings/drawings010.jpg";
+import drawings011 from "../imgs/drawings/drawings011.jpg";
+import drawings012 from "../imgs/drawings/drawings012.jpg";
+import drawings013 from "../imgs/drawings/drawings013.jpg";
+import sketches001 from "../imgs/sketches/sketches001.jpg";
+import sketches002 from "../imgs/sketches/sketches002.jpg";
+import sketches003 from "../imgs/sketches/sketches003.jpg";
+import sketches004 from "../imgs/sketches/sketches004.jpg";
+import digital001 from "../imgs/digital/digital001.jpg";
+import digital002 from "../imgs/digital/digital002.jpg";
+import digital003 from "../imgs/digital/digital003.jpg";
+import digital004 from "../imgs/digital/digital004.jpg";
+import digital005 from "../imgs/digital/digital005.jpg";
+import digital006 from "../imgs/digital/digital006.jpg";
+import digital007 from "../imgs/digital/digital007.jpg";
+import digital008 from "../imgs/digital/digital008.jpg";
+import digital009 from "../imgs/digital/digital009.jpg";
+import digital010 from "../imgs/digital/digital010.jpg";
+import digital011 from "../imgs/digital/digital011.jpg";
+import digital012 from "../imgs/digital/digital012.jpg";
+import digital013 from "../imgs/digital/digital013.jpg";
+import digital014 from "../imgs/digital/digital014.jpg";
+import digital015 from "../imgs/digital/digital015.jpg";
+
 export const Drawings = [
   {
     id: 1,
-    url: "./drawings/drawings001.jpg",
+    url: drawings001,
     text: "Drawings 1",
   },
   {
     id: 2,
-    url: "./drawings/drawings002.jpg",
+    url: drawings002,
     text: "Aranha",
   },
   {
     id: 3,
-    url: "./drawings/drawings003.jpg",
+    url: drawings003,
     text: "Mergulhador",
   },
   {
     id: 4,
-    url: "./drawings/drawings004.jpg",
+    url: drawings004,
     text: "Tiozim",
   },
   {
     id: 5,
-    url: "./drawings/drawings005.jpg",
+    url: drawings005,
     text: "Panda",
   },
   {
     id: 6,
-    url: "./drawings/drawings006.jpg",
+    url: drawings006,
     text: "Vovó",
   },
   {
     id: 7,
-    url: "./drawings/drawings007.jpg",
+    url: drawings007,
     text: "Drawings 7",
   },
   {
     id: 8,
-    url: "./drawings/drawings008.jpg",
+    url: drawings008,
     text: "Drawings 8",
   },
   {
     id: 9,
-    url: "./drawings/drawings009.jpg",
+    url: drawings009,
     text: "Drawings 9",
   },
   {
     id: 10,
-    url: "./drawings/drawings010.jpg",
+    url: drawings010,
     text: "Drawings 10",
   },
   {
     id: 11,
-    url: "./drawings/drawings011.jpg",
+    url: drawings011,
     text: "Amy",
   },
   {
     id: 12,
-    url: "./drawings/drawings012.jpg",
+    url: drawings012,
     text: "Self",
   },
   {
     id: 13,
-    url: "./drawings/drawings013.jpg",
+    url: drawings013,
     text: "Self",
   },
 ];
@@ -69,22 +102,22 @@ export const Drawings = [
 export const Sketches = [
   {
     id: 1,
-    url: "./sketches/sketches001.jpg",
+    url: sketches001,
     text: "sketches 1",
   },
   {
     id: 2,
-    url: "./sketches/sketches002.jpg",
+    url: sketches002,
     text: "sketches 2",
   },
   {
     id: 3,
-    url: "./sketches/sketches003.jpg",
+    url: sketches003,
     text: "sketches 3",
   },
   {
     id: 4,
-    url: "./sketches/sketches004.jpg",
+    url: sketches004,
     text: "sketches 4",
   },
 ];
@@ -92,77 +125,77 @@ export const Sketches = [
 export const Digital = [
   {
     id: 1,
-    url: "./digital/digital001.jpg",
+    url: digital001,
     text: "scare",
   },
   {
     id: 2,
-    url: "./digital/digital002.jpg",
+    url: digital002,
     text: "square",
   },
   {
     id: 3,
-    url: "./digital/digital003.jpg",
+    url: digital003,
     text: "square",
   },
   {
     id: 4,
-    url: "./digital/digital004.jpg",
+    url: digital004,
     text: "square",
   },
   {
     id: 5,
-    url: "./digital/digital005.jpg",
+    url: digital005,
     text: "square",
   },
   {
     id: 6,
-    url: "./digital/digital006.jpg",
+    url: digital006,
     text: "monsters",
   },
   {
     id: 7,
-    url: "./digital/digital007.jpg",
+    url: digital007,
     text: "monsters",
   },
   {
     id: 8,
-    url: "./digital/digital008.jpg",
+    url: digital008,
     text: "monsters",
   },
   {
     id: 9,
-    url: "./digital/digital009.jpg",
+    url: digital009,
     text: "monsters",
   },
   {
     id: 10,
-    url: "./digital/digital010.jpg",
+    url: digital010,
     text: "monsters",
   },
   {
     id: 11,
-    url: "./digital/digital011.jpg",
+    url: digital011,
     text: "monsters",
   },
   {
     id: 12,
-    url: "./digital/digital012.jpg",
+    url: digital012,
     text: "monsters",
   },
   {
     id: 13,
-    url: "./digital/digital013.jpg",
+    url: digital013,
     text: "monsters",
   },
   {
     id: 14,
-    url: "./digital/digital014.jpg",
+    url: digital014,
     text: "monsters",
   },
   {
     id: 15,
-    url: "./digital/digital015.jpg",
+    url: digital015,
     text: "monsters",
   },
 ];
